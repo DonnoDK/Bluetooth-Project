9e7beaed39a56f3057214bd363223bfeccc662b9
Brian Pedersen <brianfrostpedersen@gmail.com>
2012-01-20 13:46:39 +0100

    Added AppController class

---
+++ Bluetooth Project/AppController.h
@@ -0,0 +1,13 @@
+//
+//  AppController.h
+//  Bluetooth Project
+//
+//  Created by Brian Pedersen on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@end
---
+++ Bluetooth Project/AppController.m
@@ -0,0 +1,13 @@
+//
+//  AppController.m
+//  Bluetooth Project
+//
+//  Created by Brian Pedersen on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@end
