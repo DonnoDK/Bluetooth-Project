7602cbddbf3c22aa1eb819fd79049702f54fa92f
Brian Pedersen <brianfrostpedersen@gmail.com>
2012-01-20 13:15:39 +0100

    Initial Commit

---
+++ Bluetooth Project/BBAAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  BBAAppDelegate.h
+//  Bluetooth Project
+//
+//  Created by Brian Pedersen on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BBAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
