3c0914cd6e25cf2f8bc599add99a1809cc970e32
peturingi <petur@petur.eu>
2012-01-20 14:04:14 +0100

    Added preference panel and its delegates and controller

---
+++ Bluetooth Project/PreferenceController.h
@@ -0,0 +1,20 @@
+//
+//  PreferenceController.h
+//  Bluetooth Project
+//
+//  Created by Pétur Egilsson on 1/20/12.
+//  Copyright (c) 2012 Bluebird Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferenceController : NSWindowController
+
+@property NSUInteger countdownValue;
+@property BOOL lockScreen;
+@property BOOL dimDisplay;
+@property BOOL dimKeyboard;
+@property NSUInteger thresholdValue;
+@property NSUInteger signalStrength;
+
+@end
---
+++ Bluetooth Project/PreferenceController.m
@@ -0,0 +1,37 @@
+//
+//  PreferenceController.m
+//  Bluetooth Project
+//
+//  Created by Pétur Egilsson on 1/20/12.
+//  Copyright (c) 2012 Bluebird Apps. All rights reserved.
+//
+
+#import "PreferenceController.h"
+
+@implementation PreferenceController
+
+@synthesize countdownValue;
+@synthesize lockScreen;
+@synthesize dimDisplay;
+@synthesize dimKeyboard;
+@synthesize thresholdValue;
+@synthesize signalStrength;
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
