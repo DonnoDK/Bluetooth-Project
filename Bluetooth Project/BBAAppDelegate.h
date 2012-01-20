e5f824afaf5f736e9e75c756b77608713891d904
Brian Pedersen <brianfrostpedersen@gmail.com>
2012-01-20 13:33:01 +0100

    change

--- Bluetooth Project/BBAAppDelegate.h
+++ Bluetooth Project/BBAAppDelegate.h
@@ -3,7 +3,7 @@
 //  Bluetooth Project
 //
 //  Created by Brian Pedersen on 1/20/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Bluebird. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
