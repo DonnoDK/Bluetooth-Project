aa8fe4e82a9eb53f53401d274e835a31a0387556
peturingi <petur@petur.eu>
2012-01-21 12:37:21 +0100

    Preferences, Not finished but does run

--- Bluetooth Project/PreferenceController.h
+++ Bluetooth Project/PreferenceController.h
@@ -8,19 +8,41 @@
 
 #import <Cocoa/Cocoa.h>
 
+extern NSString * const BBACountDownValueKey;
+extern NSString * const BBALockScreenKey;
+extern NSString * const BBADimDisplayKey;
+extern NSString * const BBADimKeyboardKey;
+extern NSString * const BBAThresholdValueKey;
+
 @interface PreferenceController : NSWindowController <NSWindowDelegate> 
 
-@property NSUInteger countdownValue;
+@property (nonatomic) NSUInteger countdownValue;
 @property BOOL lockScreen;
-@property BOOL dimDisplay;
+@property (nonatomic) BOOL dimDisplay;
 @property BOOL dimKeyboard;
 @property NSUInteger thresholdValue;
-@property NSUInteger signalStrength;
+@property NSUInteger signalStrength; // Not stored as a default
+
++ (NSUInteger)preferenceCountDownValue;
++ (void)setPreferenceCountDownValue:(NSUInteger)value;
+
++ (BOOL)preferenceLockScreen;
++ (void)setPreferenceLockScreen:(BOOL)value;
+
++ (BOOL)preferenceDimDisplay;
++ (void)setPreferenceDimDisplay:(BOOL)value;
+
++ (BOOL)preferenceDimKeyboard;
++ (void)setPreferenceDimKeyboard:(BOOL)value;
+
++ (NSUInteger)preferenceThresholdValue;
++ (void)setPreferenceThresholdValue:(NSUInteger)value;
 
 - (IBAction)resetToDefaults:(id)sender;
 
 - (void)alertEnded:(NSAlert *)alert
               code:(NSInteger)choise
            context:(void *)v;
+
 
 @end
--- Bluetooth Project/PreferenceController.m
+++ Bluetooth Project/PreferenceController.m
@@ -8,6 +8,20 @@
 
 #import "PreferenceController.h"
 
+// Default values
+#define DEFAULT_COUNT_DOWN_VALUE    30
+#define DEFAULT_LOCK_SCREEN         TRUE
+#define DEFAULT_DIM_DISPLAY         TRUE
+#define DEFAULT_DIM_KEYBOARD        TRUE
+#define DEFAULT_THRESHOLD_VALUE     50
+
+// Holds the relevant keys in the .plist in ~/Library/Preferences/
+NSString * const BBACountDownValueKey    = @"BBACountDownValue";
+NSString * const BBALockScreenKey        = @"BBALockScreen";
+NSString * const BBADimDisplayKey        = @"BBADimDisplay";
+NSString * const BBADimKeyboardKey       = @"BBADimKeyboard";
+NSString * const BBAThresholdValueKey    = @"BBAThresholdValue";
+
 @implementation PreferenceController
 
 @synthesize countdownValue; // Seconds before actions are taken, after connection is lost
@@ -16,6 +30,70 @@
 @synthesize dimKeyboard;    // Dim keyboard?
 @synthesize thresholdValue; // Minimum signal strength before taking actions
 @synthesize signalStrength; // Current signal strength
+
+
+#pragma mark Defaults
++ (void)initialize {
+    
+    // Create a dictionary to hold the default values
+    NSMutableDictionary *defaultValues = [NSMutableDictionary dictionary];
+    
+    // Set the defaults into the directory
+    [defaultValues setObject:[NSNumber numberWithUnsignedInt:DEFAULT_COUNT_DOWN_VALUE] forKey:BBACountDownValueKey];
+    [defaultValues setObject:[NSNumber numberWithBool:DEFAULT_LOCK_SCREEN] forKey:BBALockScreenKey];
+    [defaultValues setObject:[NSNumber numberWithBool:DEFAULT_DIM_DISPLAY] forKey:BBADimDisplayKey];
+    [defaultValues setObject:[NSNumber numberWithBool:DEFAULT_DIM_KEYBOARD] forKey:BBADimKeyboardKey];
+    [defaultValues setObject:[NSNumber numberWithUnsignedInt:DEFAULT_THRESHOLD_VALUE] forKey:BBAThresholdValueKey];
+    
+    // Register the defaults
+    [[NSUserDefaults standardUserDefaults] registerDefaults:defaultValues];
+    
+#ifdef DEBUG
+    NSLog(@"initialize: registered defaults: %@", defaultValues);
+#endif
+}
+
+// Set and get defaults for count down value
++ (NSUInteger)preferenceCountDownValue {
+    return [[NSUserDefaults standardUserDefaults] integerForKey:BBACountDownValueKey];
+}
++ (void)setPreferenceCountDownValue:(NSUInteger)value {
+    [[NSUserDefaults standardUserDefaults] setInteger:value forKey:BBACountDownValueKey];
+}
+
+// Set and get defaults for lock screen
++ (BOOL)preferenceLockScreen {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:BBALockScreenKey];
+}
++ (void)setPreferenceLockScreen:(BOOL)value {
+    [[NSUserDefaults standardUserDefaults] setBool:value forKey:BBALockScreenKey];
+    
+}
+
+// Set and get defaults for dim display
++ (BOOL)preferenceDimDisplay {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:BBADimDisplayKey];
+}
++ (void)setPreferenceDimDisplay:(BOOL)value {
+    [[NSUserDefaults standardUserDefaults] setBool:value forKey:BBADimDisplayKey];
+}
+
+// Set and get defaults for dim keyboard
++ (BOOL)preferenceDimKeyboard {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:BBADimKeyboardKey];
+}
++ (void)setPreferenceDimKeyboard:(BOOL)value {
+    [[NSUserDefaults standardUserDefaults] setBool:value forKey:BBADimKeyboardKey];
+}
+
+// Set and get defaults for threshold value
++ (NSUInteger)preferenceThresholdValue {
+    return [[NSUserDefaults standardUserDefaults] integerForKey:BBAThresholdValueKey];
+}
++ (void)setPreferenceThresholdValue:(NSUInteger)value {
+    [[NSUserDefaults standardUserDefaults] setInteger:value forKey:BBAThresholdValueKey];
+}
+
 
 - (id)init {
     self = [super initWithWindowNibName:@"Preference"];
@@ -39,6 +117,12 @@
 - (void)windowDidLoad
 {
     [super windowDidLoad];
+    [self setCountdownValue:[PreferenceController preferenceCountDownValue]];
+    [self setLockScreen:[PreferenceController preferenceLockScreen]];
+    [self setDimDisplay:[PreferenceController preferenceDimDisplay]];
+    NSLog(@"Dim display is: %d", [PreferenceController preferenceDimDisplay]);
+    [self setDimKeyboard:[PreferenceController preferenceDimKeyboard]];
+    [self setThresholdValue:[PreferenceController preferenceThresholdValue]];
 }
 
 #pragma mark IBActions
@@ -71,6 +155,17 @@
         [self setCountdownValue:30];
         [self setThresholdValue:50];
     }
+}
+
+- (void)setDimDisplay:(BOOL)value {
+    [PreferenceController setPreferenceDimDisplay:value];
+    [self willChangeValueForKey:@"dimDisplay"];
+    dimDisplay = value;
+    [self didChangeValueForKey:@"dimDisplay"];
+    
+#ifdef DEBUG
+    NSLog(@"Calling setDimDisplay");
+#endif
 }
 
 @end
