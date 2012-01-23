a9a6f574022c9dcbb809ebdc27e95d273d51d00b
Brian Pedersen <brianfrostpedersen@gmail.com>
2012-01-23 13:59:04 +0100

    Changed firstTimeLaunch

--- Bluetooth Project/AppController.m
+++ Bluetooth Project/AppController.m
@@ -36,8 +36,7 @@
     // load preferences
     #ifdef DEBUG
     #endif
-
-    firstTimeLaunched = YES; // Change this to not display the setup screen at startup
+    firstTimeLaunched = NO; // Change this to not display the setup screen at startup
     // is this the first time the app is launched?
     if (firstTimeLaunched) {
         // show setup screen and prompt the user to discover/pair a device for use in the app
